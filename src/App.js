0fd151e210b6082ca38190885cf0e61c853795fb
erol23 <hurkanrol15@gmail.com>
2023-02-28 19:28:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/instructors" element={<Instructors />} /> 
+          <Route path="/contact" element={<Contact />} /> 
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/instructors">Instructors</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
